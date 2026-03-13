96eb3945ea567680f712fc60158e3d23378d7e54
Kailash Prajapati <kprajapati0417@gmail.com>
2026-03-13 19:00:44 +0530

    Add files via upload

---
+++ 18_millennium_year.py
@@ -0,0 +1,3 @@
+year = int(input("Enter Year"))
+if year % 1000 == 0:
+    print("millennium year")
---
+++ 19_elder_brother.py
@@ -0,0 +1,9 @@
+age1 =int(input("Enter First Brother Age:"))
+age2 =int(input("Enter Second Brother Age:"))
+
+if age1==age2:
+     print("Two Brother Age Same")
+elif age1 > age2:
+    print("First Brother Is Elder")
+else:
+     print("Second Brother Is Elder")   
