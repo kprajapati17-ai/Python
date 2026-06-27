324118a39f4e230191ef2577a28a67c7f8f7098a
kprajapati0417-lang <kprajapati0417@gmail.com>
2026-06-27 21:08:58 +0530

    mobile

---
+++ 11_custom_components_mobile.py
@@ -0,0 +1,42 @@
+import spacy
+import re
+from spacy.language import Language
+from spacy.tokens import Doc
+
+nlp = spacy.load("en_core_web_sm")
+
+Doc.set_extension("MobileNumbers", default=[])
+
+def get_mobile_numbers(text):
+    pattern = r'(?:\+91[\-\s]?)?[6-9]\d{9}\b'
+    return re.findall(pattern, text)
+
+@Language.component("FetchMobileNumbers")
+def FetchMobileNumbers(doc):
+    doc._.MobileNumbers = get_mobile_numbers(doc.text)
+    return doc
+
+nlp.add_pipe("FetchMobileNumbers", last=True)
+
+text = """
+
+Mobile Numbers:
+9876543210
+9123456789
++91 9876501234
++91-9988776655
+
+
+Invalid Mobile Numbers:
+1234567890
+5555555555
+98765
+11111111111
+"""
+
+doc = nlp(text)
+
+
+print("\nMobile Numbers:")
+for number in doc._.MobileNumbers:
+    print(number)
